695f16d8c097496f7104c51ca1c7166e1d8a3a03
krukl <70011865+krukl@users.noreply.github.com>
2024-02-06 02:37:18 +0200

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,18 +1,3 @@
-/*let username = document.getElementById('username')
-let submit = document.getElementById('submit')
-
-
-
-function User(username) {
-    this.username = username;
-}
-
-submit.addEventListener('click', () => {
- nameUser =  username.value;
- console.log(nameUser)
-})*/
-
-
 document.getElementById('username').oninput = function () {
     sessionStorage.setItem('my-input_save', this.value);
 }
